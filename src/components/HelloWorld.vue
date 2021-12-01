5ffeee17e853ca38184899c6ec91c385c339803c
mogata <ms46.dev@gmail.com>
2021-12-02 08:51:53 +0900

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="alert alert-primary">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+    <hr>
+    <div class="alert alert-light">
+      <h3>Inneer Slot</h3>
+      <ol class="list-group test-left">
+        <slot />
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      title: 'Slot',
+      message: 'This is message.'
+    }
+  }
+}
+</script>
